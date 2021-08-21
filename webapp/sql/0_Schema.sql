a6747ce8c8ee7233e966edc584cf506663847ad0
akihiro_nagamine <a.ki.yu.522@gmail.com>
2021-08-21 12:32:04 +0900

    indexの順番を変更

--- webapp/sql/0_Schema.sql
+++ webapp/sql/0_Schema.sql
@@ -14,8 +14,8 @@ CREATE TABLE `isu` (
   `updated_at` DATETIME(6) DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
    PRIMARY KEY(`id`)
 ) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8mb4;
-CREATE INDEX index_jia_isu_uuid_userid_userid ON isu (jia_isu_uuid,jia_user_id);
 CREATE INDEX index_isu_uuid ON isu (jia_isu_uuid);
+CREATE INDEX index_jia_isu_uuid_userid_userid ON isu (jia_isu_uuid,jia_user_id);
 
 CREATE TABLE `isu_condition` (
   `id` bigint AUTO_INCREMENT,
